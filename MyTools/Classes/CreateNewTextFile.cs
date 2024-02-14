cbf6de265788e6a6be3d29873a991cb79c05fc2f
Igor Pinheiro da Silva <igork2kta@hotmail.com>
2024-02-14 18:04:41 -0300

    melhoria no codigo

--- MyTools/Classes/CreateNewTextFile.cs
+++ MyTools/Classes/CreateNewTextFile.cs
@@ -19,7 +19,7 @@ namespace MyTools.Classes
             IntPtr handle = GetForegroundWindow();
 
             // Required ref: SHDocVw (Microsoft Internet Controls COM Object) - C:\Windows\system32\ShDocVw.dll
-            ShellWindows shellWindows = new ShellWindows();
+            ShellWindows shellWindows = new();
 
             // loop through all windows
             foreach (InternetExplorer window in shellWindows)
@@ -28,13 +28,13 @@ namespace MyTools.Classes
                 if (window.HWND == (int)handle)
                 {
                     // Required ref: Shell32 - C:\Windows\system32\Shell32.dll
-                    var shellWindow = window.Document as Shell32.IShellFolderViewDual2;
+                    // Shell32.IShellFolderViewDual2? shellWindow = window.Document as Shell32.IShellFolderViewDual2;
 
                     // will be null if you are in Internet Explorer for example
-                    if (shellWindow != null)
+                    if (window.Document is Shell32.IShellFolderViewDual2 shellWindow)
                     {
                         // Item without an index returns the current object
-                        var currentFolder = shellWindow.Folder.Items().Item();
+                        Shell32.FolderItem currentFolder = shellWindow.Folder.Items().Item();
 
                         // special folder - use window title
                         // for some reason on "Desktop" gives null
@@ -42,10 +42,10 @@ namespace MyTools.Classes
                         {
                             // Get window title instead
                             const int nChars = 256;
-                            StringBuilder Buff = new StringBuilder(nChars);
+                            StringBuilder Buff = new(nChars);
                             if (GetWindowText(handle, Buff, nChars) > 0)
                                 return Buff.ToString();
-                            
+
                         }
                         else
                             return currentFolder.Path;
@@ -59,26 +59,28 @@ namespace MyTools.Classes
         public static void Criar()
         {
             string caminho = GetActiveExplorerPath();
-            
-            if (caminho == null)
+
+            if (string.IsNullOrEmpty(caminho))
                 return;
 
             string nomeArquivo = "Novo Documento de Texto.txt";
 
             // Verifica se o arquivo base já existe
-            if (!File.Exists(Path.Combine(caminho, nomeArquivo)))
+            string caminhoCompleto = Path.Combine(caminho, nomeArquivo);
+            if (!File.Exists(caminhoCompleto))
             {
-                File.Create(Path.Combine(caminho, nomeArquivo)).Close();
+                File.Create(caminhoCompleto).Close();
                 return;
             }
+
 
             // Procura um nome alternativo
             for (int i = 1; i < 50; i++)
             {
-                caminho = Path.Combine(caminho, $"Novo Documento de Texto ({i}).txt");
-                if (!File.Exists(caminho))
+                string caminhoAlternativo = Path.Combine(caminho, $"Novo Documento de Texto ({i}).txt");
+                if (!File.Exists(caminhoAlternativo))
                 {
-                    File.Create(caminho).Close();
+                    File.Create(caminhoAlternativo).Close();
                     return;
                 }
             }
